234d225db21ff37e37702e242d80f2417751524b
Sam Hoffman <samdhoffman@gmail.com>
2020-02-12 11:37:27 -0500

    Load player data

--- src/components/Players.js
+++ src/components/Players.js
@@ -4,57 +4,28 @@ import './Players.scss';
 import PlayerCard from './PlayerCard';
 
 const Players = () => {
-  const [playerData, setPlayerData] = useState({});
-  const [shuffledPlayers, setShuffledPlayers] = useState([]);
-  const [guessCount, setGuessCount] = useState(0);
+  const [playerData, setPlayerData] = useState(null);
+  
+  const PLAYER_CARD_COUNT = 4;
   const URL = "https://gist.githubusercontent.com/liamjdouglas/bb40ee8721f1a9313c22c6ea0851a105/raw/6b6fc89d55ebe4d9b05c1469349af33651d7e7f1/Player.json"
 
-  const loadPlayerData = () => {
-    axios.get(URL)
-      .then(res => {
-        console.log(res)
-        setPlayerData(res);
-        shufflePlayers(res.data.players);
-      })
-      .catch(err => console.log(err))
-  }
-
-  useEffect(() => { loadPlayerData() }, []);
-
-  const shufflePlayers = (arr) => {
-    var copy = arr.slice(0);
-    let i = copy.length, randomIndex, temp;
-
-    while (--i > 0) {
-      randomIndex = Math.floor(Math.random() * (i+1));
-      temp = copy[randomIndex];
-      copy[randomIndex] = copy[i];
-      copy[i] = temp;
+  const loadPlayerData = async () => {
+    try {
+      const response = await axios.get(URL);
+      setPlayerData(response);
+    } catch(error) {
+      console.log(error);
     }
-
-    setShuffledPlayers(copy);
   }
 
-  function randomNoRepeats(array) {
-    var copy = array.slice(0);
-    return function() {
-      if (copy.length < 1) { copy = array.slice(0); }
-      var index = Math.floor(Math.random() * copy.length);
-      var item = copy[index];
-      copy.splice(index, 1);
-      return item;
-    };
-  }
-
-  const makeGuess = () => {
-    setGuessCount(guessCount + 1);
-    shufflePlayers(playerData.data.players);
-  }
+  useEffect(() => { 
+    loadPlayerData();
+  }, []);
 
   return (
     <div className="Players">
-      <PlayerCard playerData={playerData} makeGuess={makeGuess} />
-      <PlayerCard makeGuess={makeGuess} />
+      <section className="card-container">
+      </section>
     </div>
   )
 }
