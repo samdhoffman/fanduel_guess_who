273db65e2871a37e5139a0c357b9b253b3dd4cf4
Sam Hoffman <samdhoffman@gmail.com>
2020-02-13 12:07:32 -0500

    Update styling for player card and game

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -5,10 +5,9 @@ const PlayerCard = ({playerData, makeGuess}) => {
   return (
     <div className="PlayerCard" data-test="PlayerCard">
       <div className="player-info">
-        <h2>Player Card</h2>
-        <header data-test="player-name">{playerData.first_name} {playerData.last_name}</header>
+        <header data-test="player-name" className="player-name">{playerData.first_name} {playerData.last_name}</header>
         <img data-test="player-img" className="player-img" src={playerData.images.default.url} alt="player"/>
-        <button onClick={() => makeGuess(playerData.id)}>Make Guess</button>
+        <button className="guess-btn" onClick={() => makeGuess(playerData.id)}>Make Guess</button>
       </div>
     </div>
   )
