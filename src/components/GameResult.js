eb568b64d36b6a2bd6ff1bd953763254a2e49cfa
Sam Hoffman <samdhoffman@gmail.com>
2020-02-13 15:31:54 -0500

    Add confetti to GameResult page

--- src/components/GameResult.js
+++ src/components/GameResult.js
@@ -1,9 +1,15 @@
 import React from "react"
 import "./GameResult.scss";
 
+import Confetti from 'react-confetti'
+
 const GameResult = ({resetGame}) => {
   return (
     <div className="GameResult">
+      <Confetti
+        numberOfPieces={500}
+        recycle={false}
+      />
       <section>
         <header>Congrats! You have successfully gussed 10 players with the highest Fanduel Points Per Game!</header>
         <p>To restart the game please click the button below. Happy guessing!</p>
